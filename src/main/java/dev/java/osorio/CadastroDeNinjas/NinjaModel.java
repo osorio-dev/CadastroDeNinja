7f8def33726c43660d961bf16c4f6f7a401b1384
osorio-dev <feeh.osorio.dev@gmail.com>
2025-05-19 14:48:24 -0300

    add getters and setters

---
+++ src/main/java/dev/java/osorio/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,54 @@
+package dev.java.osorio.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private int age;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
